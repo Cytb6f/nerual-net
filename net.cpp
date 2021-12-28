8fa5381519343d47689beea641251872c862f07e
Cytb6f <Cytb6f@LAPTOP-48RC0Q42>
2021-12-28 14:08:38 +0800

    添加项目文件。

---
+++ net.cpp
@@ -0,0 +1,70 @@
+#include "net.h"
+#include <thread>
+#include <iostream>
+
+
+const std::string      trainfile = "train.txt";       //训练数据地址
+const std::string        prefile = "pre.txt";         //预测数据地址
+const std::string       logofile = "logo.txt";        //日志数据地址
+const std::string       costfile1 = "C:\\Users\\Cytb6f\\Desktop\\信工1901-2019040255-王丹阳-第1次作业\\cost1.txt";        //成本数据地址
+const std::string       costfile2 = "C:\\Users\\Cytb6f\\Desktop\\信工1901-2019040255-王丹阳-第1次作业\\cost2.txt";        //成本数据地址
+const std::string       costfile3 = "C:\\Users\\Cytb6f\\Desktop\\信工1901-2019040255-王丹阳-第1次作业\\cost3.txt";        //成本数据地址
+const std::string       costfile4 = "C:\\Users\\Cytb6f\\Desktop\\信工1901-2019040255-王丹阳-第1次作业\\cost4.txt";        //成本数据地址
+
+std::vector<unsigned long> layernode= {2, 6,16, 1};
+
+void f1() {
+	net bpnn;
+	bpnn.load_trainset(trainfile);
+	bpnn.set_layernum(layernode.size());
+	bpnn.set_layernode(layernode);
+	bpnn.set_alpha(0.001);
+	bpnn.train(5000, (train_type)1);
+	bpnn.download(costfile1);
+	return;
+}
+void f2() {
+	net bpnn;
+	bpnn.load_trainset(trainfile);
+	bpnn.set_layernum(layernode.size());
+	bpnn.set_layernode(layernode);
+	bpnn.set_alpha(0.001);
+	bpnn.train(5000, (train_type)2);
+	bpnn.download(costfile2);
+	return;
+}
+void f3() {
+	net bpnn;
+	bpnn.load_trainset(trainfile);
+	bpnn.set_layernum(layernode.size());
+	bpnn.set_layernode(layernode);
+	bpnn.set_alpha(0.001);
+	bpnn.train(5000, (train_type)3);
+	bpnn.download(costfile3);
+	return;
+}
+void f4() {
+	net bpnn;
+	bpnn.load_trainset(trainfile);
+	bpnn.set_layernum(layernode.size());
+	bpnn.set_layernode(layernode);
+	bpnn.set_alpha(0.001);
+	bpnn.train(5000, (train_type)4);
+	bpnn.download(costfile4);
+	return;
+}
+
+int main() {
+
+	std::thread th1(f1);
+	std::thread th2(f2);
+	std::thread th3(f3);
+	std::thread th4(f4);
+
+	th1.join();
+	th2.join();
+	th3.join();
+	th4.join();
+	
+	return 0;
+}
